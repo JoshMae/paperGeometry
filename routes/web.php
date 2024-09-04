7514dc8b6fcb9238bc3603a31473d99a79e8388d
Carloxx18 <90119210+Carloxx18@users.noreply.github.com>
2024-09-03 22:53:58 -0600

    Prueba

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ Route::get('/', function () {
     return "HOLA MUNDO";
 });
 
-<<<<<<< HEAD
+
 // Ruta en web.php
 Route::get('/carrito', function () {
     return view('carrito');
 });
-=======
+
 Route::get('/productos', [CatalogoController::class, 'index']);
  
->>>>>>> 4110e25aca71448bfbb0e1d1b6e0a69609b02fe8
+
